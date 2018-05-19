7abc7103098b152768983e11d1064b1423023c85
Chris <chrisrkemp@hotmail.co.uk>
2018-05-19 11:20:22 +0100

    Updated gitignore, added camcontrol
    Update gitignore to ignore the visual studio settings files

---
+++ LimitedPoaaaah/Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class controls the camera movement; camera will follow the followtarget
+
+public class CameraControl : MonoBehaviour {
+
+    //Variables and references ~~~
+
+    //References
+    [Header("References")]
+    public Transform _followTarget;
+
+    //Properties
+    [Header("Properties")]
+    public float _leftBound;
+    public float _rightBound;
+    public float _topBound;
+    public float _bottomBound;
+    public float _trailDistance;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
