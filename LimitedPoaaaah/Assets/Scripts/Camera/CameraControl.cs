82304b5f4267a1c9687b0f5b84d5ad69fa271e38
Chris <chrisrkemp@hotmail.co.uk>
2018-05-19 11:53:44 +0100

    Added camera control script and prefab
    Camera follows a target at a certain speed, currently doesn't stay in bounds

--- LimitedPoaaaah/Assets/Scripts/Camera/CameraControl.cs
+++ LimitedPoaaaah/Assets/Scripts/Camera/CameraControl.cs
@@ -14,19 +14,59 @@ public class CameraControl : MonoBehaviour {
 
     //Properties
     [Header("Properties")]
-    public float _leftBound;
-    public float _rightBound;
-    public float _topBound;
-    public float _bottomBound;
-    public float _trailDistance;
+    public float _leftBound = 0f;
+    public float _rightBound = 10f;
+    public float _topBound = 10f;
+    public float _bottomBound = 0f;
+    public float _trailDistance = 1f;
+    public float _speed = 5f;
+    public float _camHeight = 10f;
+    float _minSpeed;
+    float _angle;
+    Vector3 _offset;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
+    //Initialisation ~~~
+
+    void Start()
+    {
+        _minSpeed = _speed / 2.0f;
+        _angle = Mathf.Deg2Rad * Camera.main.transform.eulerAngles.x;
+        _offset = new Vector3(0f, _camHeight, -_camHeight * Mathf.Tan(_angle));
+    }
+    public void SetSpeed(float speed)
+    {
+        _speed = speed;
+    }
+    public void SetSpeedToPlayer()
+    {
+        //TODO: Set max speed to player speed
+        //player p = _followTarget.gameObject.GetComponent<player>();
+        //SetSpeed(p.speed);
+    }
+
+    //Update ~~~
 	
-	// Update is called once per frame
 	void Update () {
-		
+
+        //Calculate the displacement and distance between camera and target
+        Vector3 targetPos = _followTarget.position + _offset;
+        Vector3 displacement = targetPos - transform.position;
+        float distance = displacement.magnitude;
+
+        //Calculate speed and movement vector
+        float speed = Mathf.Lerp(_minSpeed, _speed, distance / _trailDistance);
+        Vector3 movement = displacement.normalized * speed * Time.deltaTime;
+
+        //If movement vector is larget than distance between target and camera then set position to target
+        if(movement.magnitude > distance)
+        {
+            transform.position = targetPos;
+        }
+        else
+        {
+            transform.position += movement;
+        }
+
+        //TODO: Add boundary constraints
 	}
 }
