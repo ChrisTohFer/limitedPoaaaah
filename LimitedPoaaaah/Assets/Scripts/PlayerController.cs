cbe6f730f61c6c7a09f0a3f6cca55d67c7ce395d
pratledge <phratledge@hotmail.co.uk>
2018-05-19 13:43:23 +0100

    materials and player control
    made changes to player control script,

---
+++ LimitedPoaaaah/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public float speed;
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, z);
+    }
+}
