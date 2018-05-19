087db56a7c6315df700f6eda5582bf76e1557763
Chris <chrisrkemp@hotmail.co.uk>
2018-05-19 19:13:07 +0100

    Added enemy death, omochao character

---
+++ LimitedPoaaaah/Assets/Scripts/Projectiles/PlayerProj.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProj : MonoBehaviour {
+
+    //Properties
+
+    public float _damage;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            EnemyHealth enemy = other.GetComponent<EnemyHealth>();
+            enemy.Damage(_damage);
+            gameObject.SetActive(false);
+        }
+    }
+
+}
