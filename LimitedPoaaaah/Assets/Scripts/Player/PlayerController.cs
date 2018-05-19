a380069eeb553f8976c2ce4ac78e4ac71de0c459
Unknown <phratledge@hotmail.co.uk>
2018-05-19 18:47:42 +0100

    New player Movement script
    Taken new script from unity tutorial.  requires some amending

--- LimitedPoaaaah/Assets/Scripts/Player/PlayerController.cs
+++ LimitedPoaaaah/Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float speed =20;
     void Update()
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
