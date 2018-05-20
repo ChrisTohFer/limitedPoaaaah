fcdae89dfba3960940a2b050a714b97a6fbde6a8
Unknown <phratledge@hotmail.co.uk>
2018-05-20 11:16:41 +0100

    Auto stash before merge of "development" and "origin/development"

---
+++ LimitedPoaaaah/Assets/Scripts/Player/TracksMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracksMovement : MonoBehaviour
+{
+
+    public float speed = 20;
+    void Update()
+    {
+        //Vector3 position;
+
+        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed + transform.position.x;
+        //var z = Input.GetAxis("Vertical") * Time.deltaTime * speed + transform.position.z;
+
+        // movement = new Vector3(x, 0, z);
+        // movement.Normalize()
+
+        /**transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, z); **/
+        //transform.position = new Vector3(x, transform.position.y, z);
+        transform.rotation = Quaternion.LookRotation(transform.position);
+    }
+}
