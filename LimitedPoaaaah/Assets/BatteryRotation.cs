7eac5781c6ce7a3a27a6723617d5301a0a4303e1
Lawrence Shahid <lawshahid@gmail.com>
2018-05-20 10:52:03 +0100

    Created Battery & Script to Rotate during Runtime

---
+++ LimitedPoaaaah/Assets/BatteryRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryRotation : MonoBehaviour {
+
+    public GameObject Battery;
+
+    public float RotationSpeed = 1;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Battery.transform.Rotate(0, RotationSpeed, 0);
+		
+	}
+}
