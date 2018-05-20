36179d8c4322061b7f3a22ac7009b0ab04fd17f2
Chris <chrisrkemp@hotmail.co.uk>
2018-05-20 11:26:16 +0100

    Updated enemy spawner to be wave based

--- LimitedPoaaaah/Assets/Scripts/Enemies/EnemySpawnController.cs
+++ LimitedPoaaaah/Assets/Scripts/Enemies/EnemySpawnController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawnController : MonoBehaviour {
 
@@ -9,33 +10,90 @@ public class EnemySpawnController : MonoBehaviour {
     //References
     [Header("Component References")]
     public EnemySpawner _spawner;
+    public ObjectPool _enemyPool;
+    public Text _waveCounter;
+    public Text _waveTimer;
+
+    //Properties
+    [Header("Properties")]
+    public float _enemiesPerWave = 10;
+    public float _enemiesFlatIncrease = 0f;
+    public float _enemiesProportionalIncrease = 1.5f;
+    public float _waveDuration = 10.0f;
+    public float _durationFlatIncrease = 0f;
+    public float _durationProportionalIncrease = 1.2f;
+    public float _breakTime = 3.0f;
+
+    //Other settings
+    [Header("Other settings")]
+    public float _mapSizeX = 40f;
+    public float _mapSizeY = 40f;
 
     //Variables
-    public float _chargeEnemySpawnTime = 5;
-    public float _spawnTimeReduction = 0.95f;
-    float _chargeEnemyCooldown;
+    float _duration;
+    float _count;
+    float _amplitude;
+
+    float _enemyPotential = 0f;
+    public float _timer = 0f;
+    private int _wave = 0;
 
     //Initialisation ~~~
 
     void Start()
     {
-        _chargeEnemyCooldown = _chargeEnemySpawnTime;
+        _duration = _waveDuration;
+        _count = _enemiesPerWave;
+        CalculateAmplitude();
+    }
+    //Calculate the function amplitude constant
+    void CalculateAmplitude()
+    {
+        _amplitude = _count * 2.0f / _duration;
     }
 
     //Update ~~~
 
     void FixedUpdate () {
-		if(_chargeEnemyCooldown < 0f)
-        { 
-            _chargeEnemyCooldown = _chargeEnemySpawnTime = _chargeEnemySpawnTime * _spawnTimeReduction;
-            _spawner.CircleSpawn(EnemySpawner.EnemyType.CHARGE_ENEMY, Vector3.zero, 20f);
-            if (Random.value > 0.8f)
-                _spawner._chargeEnemyPool._max++;
-        }
-        else
+        //For positive times, wave is active
+        if(_timer >= 0f)
         {
-            _chargeEnemyCooldown -= Time.fixedDeltaTime;
+            _enemyPotential += WaveFunction();
+            while(_enemyPotential >= 1.0f)
+            {
+                _enemyPotential -= 1f;
+                _spawner.SquareSpawn(EnemySpawner.EnemyType.CHARGE_ENEMY, Vector3.zero, _mapSizeX, _mapSizeY);
+            }
+
+            if(_timer >= _duration)
+            {
+                _duration *= _durationProportionalIncrease;
+                _duration += _durationFlatIncrease;
+                _count *= _enemiesProportionalIncrease;
+                _count += _enemiesFlatIncrease;
+
+                _enemyPool._max = (int)(_count * _enemiesProportionalIncrease);
+                CalculateAmplitude();
+
+                _timer = -_breakTime;
+                _wave++;
+            }
         }
 
+        UpdateUI();
+        _timer += Time.fixedDeltaTime;
+
 	}
+
+    //Methods ~~~
+
+    float WaveFunction()
+    {
+        return _amplitude * Mathf.Pow(Mathf.Sin(Mathf.PI * _timer / _duration), 2.0f) * Time.fixedDeltaTime * 1.01f;
+    }
+    void UpdateUI()
+    {
+        _waveTimer.text = "Wave Timer: " + (int)_timer;
+        _waveCounter.text = "Wave: " + _wave;
+    }
 }
